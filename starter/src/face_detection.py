72da0bd9fce38d9c421e1d3f008c4b3f6ce54261
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-14 11:10:09 +0530

    changed class name

--- starter/src/face_detection.py
+++ starter/src/face_detection.py
@@ -3,10 +3,13 @@ This is a sample class for a model. You may choose to use it as-is or make any c
 This has been provided just to give you an idea of how to structure your model class.
 '''
 
-class Model_X:
+
+class Model_Face_detection:
     '''
     Class for the Face Detection Model.
     '''
+    
+
     def __init__(self, model_name, device='CPU', extensions=None):
         '''
         TODO: Use this to set your instance variables.
@@ -32,15 +35,15 @@ class Model_X:
         raise NotImplementedError
 
     def preprocess_input(self, image):
-    '''
-    Before feeding the data into the model for inference,
-    you might have to preprocess it. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        '''
+        Before feeding the data into the model for inference,
+        you might have to preprocess it. This function is where you can do that.
+        '''
+    raise NotImplementedError
 
     def preprocess_output(self, outputs):
-    '''
-    Before feeding the output of this model to the next model,
-    you might have to preprocess the output. This function is where you can do that.
-    '''
-        raise NotImplementedError
+        '''
+        Before feeding the output of this model to the next model,
+        you might have to preprocess the output. This function is where you can do that.
+        '''
+    raise NotImplementedError
