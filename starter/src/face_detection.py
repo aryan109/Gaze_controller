42458ec0c70e47a141225b20126ad56d39b587d1
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-14 11:27:09 +0530

    Implemented init, load_model, preprocess_input

--- starter/src/face_detection.py
+++ starter/src/face_detection.py
@@ -2,19 +2,36 @@
 This is a sample class for a model. You may choose to use it as-is or make any changes to it.
 This has been provided just to give you an idea of how to structure your model class.
 '''
-
+from openvino.inference_engine import IENetwork, IECore
+import os
+import cv2
+import argparse
+import sys
+import numpy as np
+import time
 
 class Model_Face_detection:
     '''
     Class for the Face Detection Model.
     '''
-    
+
 
     def __init__(self, model_name, device='CPU', extensions=None):
         '''
         TODO: Use this to set your instance variables.
         '''
-        raise NotImplementedError
+        self.model_name = model_name
+        self.model_weights=model_name+'.bin'
+        self.model_structure=model_name+'.xml'
+        self.device=device
+        try:
+            self.model=IENetwork(self.model_structure, self.model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
+        self.input_name=next(iter(self.model.inputs))
+        self.input_shape=self.model.inputs[self.input_name].shape
+        self.output_name=next(iter(self.model.outputs))
+        self.output_shape=self.model.outputs[self.output_name].shape
 
     def load_model(self):
         '''
@@ -22,7 +39,9 @@ class Model_Face_detection:
         This method is for loading the model to the device specified by the user.
         If your model requires any Plugins, this is where you can load them.
         '''
-        raise NotImplementedError
+        core = IECore()
+        self.net = core.load_network(network=self.model, device_name=self.device, num_requests=1)
+        
 
     def predict(self, image):
         '''
@@ -39,7 +58,11 @@ class Model_Face_detection:
         Before feeding the data into the model for inference,
         you might have to preprocess it. This function is where you can do that.
         '''
-    raise NotImplementedError
+        net_input_shape = self.input_shape
+        p_image=cv2.resize(image, (net_input_shape[3], net_input_shape[2]))
+        p_image=p_image.transpose((2,0,1))
+        p_image = p_image.reshape(1, *p_image.shape)
+        return p_image
 
     def preprocess_output(self, outputs):
         '''
