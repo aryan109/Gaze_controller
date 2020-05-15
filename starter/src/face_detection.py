08e39291df23da5e7b6197adf801eb7da4f72f0c
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-15 12:23:31 +0530

    Add call to crop_inage() in face_detection.py

--- starter/src/face_detection.py
+++ starter/src/face_detection.py
@@ -80,8 +80,9 @@ class Model_Face_detection:
             if conf >= self.threshold:
 
                 coords = self.preprocess_output(box, initial_dims)
-
                 image = self.draw_outputs(coords, image)
+                cropped_image = self.crop_image(image, coords)
+                cv2.imshow("cropped image", cropped_image)
         return image
 
     # def check_model(self): # todo fill this method
