3683973ae8a02e1d5b3f5817e7769cae3ee5f248
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-20 11:52:38 +0530

    commented unnecessary parts

--- starter/src/test_combined.py
+++ starter/src/test_combined.py
@@ -20,12 +20,12 @@ video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 output_video_w = int(300)
 output_video_h = int(450)
-output_path = './output/'
-out_video = cv2.VideoWriter(os.path.join(output_path, 'output_videocom.mp4'),
-                            cv2.VideoWriter_fourcc(*'avc1'), 
-                            fps,
-                            (output_video_w, output_video_h),
-                            True)
+# output_path = './output/'
+# out_video = cv2.VideoWriter(os.path.join(output_path, 'output_videocom.mp4'),
+#                             cv2.VideoWriter_fourcc(*'avc1'), 
+#                             fps,
+#                             (output_video_w, output_video_h),
+#                             True)
 try:
     # print('inside try')
     while cap.isOpened():
@@ -39,10 +39,12 @@ try:
         cropped_image = fd.predict(frame, initial_dims) 
         # print("cropped image is "+str(cropped_image.shape))#373, 237
         # break
-        resized_cropped_image = fd.reshape_after_crop(cropped_image= cropped_image,
-                                                     width= output_video_w,
-                                                     height= output_video_h)
-        out_video.write(resized_cropped_image)
+        # resized_cropped_image = fd.reshape_after_crop(cropped_image= cropped_image,
+        #                                              width= output_video_w,
+        #                                              height= output_video_h)
+        
+
+        # out_video.write(resized_cropped_image)
 
 
     cap.release()
