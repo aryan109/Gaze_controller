1f6dd55618ef6b6476a97481eb93ec5c89e508a2
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-20 12:17:55 +0530

    Add head pose estimation to combined

--- starter/src/test_combined.py
+++ starter/src/test_combined.py
@@ -1,15 +1,20 @@
 import starter.src.face_detection as FD
 import starter.src.facial_landmarks_detection as FLD
+import starter.src.head_pose_estimation as HPE
 import cv2 
 import os   
 
 model_name1 = '/home/aryan/gaze_controller/models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001'
 model_name2 = '/home/aryan/gaze_controller/models/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-retail-0009'
+model_name3 = '/home/aryan/gaze_controller/models/intel/head-pose-estimation-adas-0001/FP32/head-pose-estimation-adas-0001'
 
 fd = FD.Model_Face_detection(model_name1, 'CPU')
 fld = FLD.Model_Facial_landmarks_de(model_name2, 'CPU')
+hpe = HPE.Model_Head_pose_estimation(model_name3, 'CPU')
+
 fd.load_model()
 fld.load_model()
+hpe.load_model()
 video_file_path = '/home/aryan/gaze_controller/starter/bin/demo.mp4'
 try:
     cap = cv2.VideoCapture(video_file_path)
@@ -23,14 +28,14 @@ initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 initial_dims = [initial_h,initial_w]
 video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 fps = int(cap.get(cv2.CAP_PROP_FPS))
-# output_video_w = int(300)
-# output_video_h = int(450)
-# output_path = './output/'
-# out_video = cv2.VideoWriter(os.path.join(output_path, 'output_videocom.mp4'),
-#                             cv2.VideoWriter_fourcc(*'avc1'), 
-#                             fps,
-#                             (output_video_w, output_video_h),
-#                             True)
+output_video_w = int(300)
+output_video_h = int(450)
+output_path = './output/'
+out_video = cv2.VideoWriter(os.path.join(output_path, 'comb_output.mp4'),
+                            cv2.VideoWriter_fourcc(*'avc1'), 
+                            fps,
+                            (output_video_w, output_video_h),
+                            True)
 try:
     # print('inside try')
     while cap.isOpened():
@@ -43,14 +48,15 @@ try:
         # print(initial_dims)#1080,1920
         cropped_image = fd.predict(frame, initial_dims) 
         real_face_coords, face_point_drawn_frame = fld.predict(cropped_image, initial_dims)
+        head_pose_angles = hpe.predict(frame, initial_dims)
         # print("cropped image is "+str(cropped_image.shape))#373, 237
         # break
-        # resized_cropped_image = fd.reshape_after_crop(cropped_image= cropped_image,
-        #                                              width= output_video_w,
-        #                                              height= output_video_h)
+        face_point_drawn_frame = fd.reshape_after_crop(cropped_image= face_point_drawn_frame,
+                                                     width= output_video_w,
+                                                     height= output_video_h)
         
 
-        # out_video.write(resized_cropped_image)
+        out_video.write(face_point_drawn_frame)
 
 
     cap.release()
