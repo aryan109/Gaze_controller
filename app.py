5b829e5c4b82339d326f4742cdf925485dfdeeeb
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-23 12:38:46 +0530

    Add functions

--- app.py
+++ app.py
@@ -5,3 +5,24 @@ import starter.src.gaze_estimation as GME
 import starter.src.mouse_controller as MC
 import cv2
 import os
+
+def resize_image(image, width, height):
+        dims = (width, height)
+        resized_image = cv2.resize(image, dims, interpolation = cv2.INTER_AREA) 
+        return resized_image
+
+def crop_image(image, coords):
+        xmin = int(coords[0])
+        ymin = int(coords[1])
+        xmax = int(coords[2])
+        ymax = int(coords[3])
+        cropped_image = image[ymin:ymax, xmin:xmax]
+        return cropped_image
+
+def generate_rectangle_coordinates_from_midpoint(x, y, delta, maxlim):
+    xmin = max(x - delta,0)
+    ymin = max(y - delta,0)
+    xmax = min(x + delta, maxlim)
+    ymax = min(y + delta, maxlim)
+    rect_coords = [xmin, ymin, xmax, ymax]
+    return rect_coords
