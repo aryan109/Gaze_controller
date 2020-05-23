5afcfdba6a494ff6ad65037db3489bf29b832800
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-23 12:57:56 +0530

    Add models

--- app.py
+++ app.py
@@ -34,9 +34,11 @@ def get_args():
     '''
     parser = argparse.ArgumentParser("Run inference on an input video")
 
-    p_desc = "Add precision of model(FP32, FP16)"
+    p_desc = "Add precision of model(FP32, FP16, FP16-INT8)"
     i_desc = "Path to input video file"
-    
+    mp_desc = "Precission of mouse controller(high, medium, low)"
+    ms_desc = "Speed of mouse controller(high, medium, low)"
+
     parser._action_groups.pop()
 
     required = parser.add_argument_group('required arguments')
@@ -45,12 +47,25 @@ def get_args():
     # -- Create the arguments
     required.add_argument("-p", help=p_desc, required=False, default = 'FP32')
     optional.add_argument("-i", help=i_desc, default='./starter/bin/demo.mp4')
+    optional.add_argument("-mp", help=mp_desc, default='medium')
+    optional.add_argument("-ms", help=ms_desc, default='medium')
+
     args = parser.parse_args()
 
     return args
 
 def main():
     args = get_args()
+    model_name1 = './models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-binary-0001'
+    model_name2 = './models/intel/landmarks-regression-retail-0009/'+args.p+'/landmarks-regression-retail-0009'
+    model_name3 = './models/intel/head-pose-estimation-adas-0001/'+args.p+'/head-pose-estimation-adas-0001'
+    model_name4 = './models/intel/gaze-estimation-adas-0002/'+args.p+'/gaze-estimation-adas-0002'
+    fd = FD.Model_Face_detection(model_name1, 'CPU')
+    fld = FLD.Model_Facial_landmarks_de(model_name2, 'CPU')
+    hpe = HPE.Model_Head_pose_estimation(model_name3, 'CPU')
+    gme = GME.Model_Gaze_estimation(model_name4, 'CPU')
+    mc = MC.MouseController(precision= args.mp, speed=args.ms)
+
 
 if __name__ == "__main__":
     main()
