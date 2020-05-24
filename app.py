9fd4e110fff4eca724a97a35453e9ffecda828ce
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-24 16:25:27 +0530

    Completed app.py

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def get_args():
     i_desc = "Path to input video file"
     mp_desc = "Precission of mouse controller(high, medium, low)"
     ms_desc = "Speed of mouse controller(high, medium, low)"
+    c_desc = "enter 1 if use webcam else 0, default is 0"
 
     parser._action_groups.pop()
 
@@ -49,6 +50,7 @@ def get_args():
     optional.add_argument("-i", help=i_desc, default='./starter/bin/demo.mp4')
     optional.add_argument("-mp", help=mp_desc, default='medium')
     optional.add_argument("-ms", help=ms_desc, default='medium')
+    optional.add_argument("-c", help=ms_desc, default=0)
 
     args = parser.parse_args()
 
@@ -69,7 +71,67 @@ def main():
     fld.load_model()
     hpe.load_model()
     gme.load_model()
-    
+    if(args.c != 1):
+        video_file_path = args.i
+    else :
+        video_file_path = 0
+    try:
+        cap = cv2.VideoCapture(video_file_path)
+        print('captured video')
+    except FileNotFoundError:
+        print("Cannot locate video file: " + video_file_path)
+    except Exception as e:
+        print("Something else went wrong with the video file: ", e)
+    initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    initial_dims = [initial_h, initial_w]
+    video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    output_video_w = int(300)
+    output_video_h = int(450)
+    delta = 60
+    try:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            cropped_image = fd.predict(frame, initial_dims)
+
+            resized_cropped_image = fd.reshape_after_crop(cropped_image=cropped_image,
+                                                           width=output_video_w,
+                                                           height=output_video_h)
+
+            real_face_coords = fld.predict(
+                resized_cropped_image, [output_video_h, output_video_w])
+
+            left_eye_coords = real_face_coords[:2]
+            right_eye_coords = real_face_coords[2:4]
+
+            left_eye_rect_coords = generate_rectangle_coordinates_from_midpoint(
+                                    left_eye_coords[0],
+                                    left_eye_coords[1],
+                                    delta,
+                                    300)
+            right_eye_rect_coords = generate_rectangle_coordinates_from_midpoint(
+                                    right_eye_coords[0], 
+                                    right_eye_coords[1], 
+                                    delta,
+                                    300)
+
+            left_eye_frame = crop_image(resized_cropped_image,left_eye_rect_coords)
+            right_eye_frame = crop_image(resized_cropped_image,right_eye_rect_coords)
+            resized_left_eye_frame = resize_image(left_eye_frame,2*delta,2*delta)
+            resized_right_eye_frame = resize_image(right_eye_frame,2*delta,2*delta)
+
+            head_pose_angles = hpe.predict(resized_cropped_image, [output_video_h, output_video_w])
+
+            gaze_result =gme.predict(head_pose_angles,resized_left_eye_frame,resized_right_eye_frame)
+
+
+            mc.move(gaze_result[0][0], gaze_result[0][1])
+            print('pointer moved')
+
 
 if __name__ == "__main__":
     main()
