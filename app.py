3aa6a25576051de511a680238de51f1e6fa495ce
Aryan Pegwar <aryan.1rn17cs019@gmail.com>
2020-05-26 10:35:38 +0530

    Add loging mechanism

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import starter.src.mouse_controller as MC
 import cv2
 import os
 import argparse
+import logging
+logging.basicConfig(filename='app.log', filemode='w') # if you want to log it into file
 
 
 def denormalize_coordinates(box, initial_w,initial_h):
@@ -139,10 +141,13 @@ def main():
     try:
         cap = cv2.VideoCapture(video_file_path)
         print('captured video')
+        logging.info("captured video")
     except FileNotFoundError:
         print("Cannot locate video file: " + video_file_path)
+        logging.info("Cannot locate video file: " + video_file_path)
     except Exception as e:
         print("Something else went wrong with the video file: ", e)
+        logging.info("Something else went wrong with the video file: ", e)
     initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     initial_dims = [initial_h, initial_w]
@@ -238,13 +243,16 @@ def main():
                                     right_eye_coords[0] + x_move + xmin_offset,
                                     right_eye_coords[1] + y_move + ymin_offset)
             print('pointer moved')
+            logging.info("pointer moved")
             if args.of :
                 out_video.write(output_image)
                 print('printed output')
+                logging.info("Printed output to output video file")
         cap.release()
         cv2.destroyAllWindows()
     except Exception as e:
         print("Could not run Inference: ", e)
+        logging.info("Could not run Inference: "+str(e))
 
 if __name__ == "__main__":
     main()
